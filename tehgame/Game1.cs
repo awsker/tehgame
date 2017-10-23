1c9cdd444bbf7d303f151971b9edfdcc343254b4
awsker <awsker@outlook.com>
2017-10-23 19:23:06 +0200

    Test

--- tehgame/Game1.cs
+++ tehgame/Game1.cs
@@ -73,7 +73,7 @@ namespace tehgame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Pink);
 
             // TODO: Add your drawing code here
 
