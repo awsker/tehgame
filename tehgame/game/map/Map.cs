ac3c7f33ee1194228d94e7c78ba76f13cb51275a
awsker <awsker>
2017-10-23 22:11:19 +0200

    Added start of Map classes

--- tehgame/game/map/Map.cs
+++ tehgame/game/map/Map.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tehgame.game.map
+﻿namespace tehgame.game.map
 {
     public class Map
     {
+        private readonly ItemGrid<Tile> _tiles;
+        private readonly ItemGrid<Wall> _walls;
 
+        public Map(int xSize, int ySize)
+        {
+            _tiles = new ItemGrid<Tile>(xSize, ySize);
+            _walls = new ItemGrid<Wall>(xSize*2 + 1, ySize*2 + 1);
+        }
+
+        public void PasteInto(Map target, int xOffset, int yOffset)
+        {
+            _tiles.PasteInto(target._tiles, xOffset, yOffset);
+            _walls.PasteInto(target._walls, xOffset, yOffset);
+        }
+        
+        public ItemGrid<Tile> Tiles => _tiles;
+        public ItemGrid<Wall> Walls => _walls;
     }
 }
---
+++ tehgame/game/map/MapGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace tehgame.game.map
+{
+    public abstract class MapGenerator
+    {
+        public abstract Map GenerateMap(int xSize, int ySize);
+
+        public Map CreateRoom(int xSize, int ySize, Tile floor)
+        {
+            var room = new Map(xSize, ySize);
+            for(int x = 0; x < xSize; ++x)
+            {
+                for(int y = 0; y < ySize; ++y)
+                {
+                    var wallPositions = new WallPositions(x, y);
+                    room.Tiles[x, y] = floor;
+                    if (x == 0)
+                        room.Walls[wallPositions.WestWall] = Wall.Vertical;
+                    if (y == 0)
+                        room.Walls[wallPositions.NorthWall] = Wall.Horizontal;
+                    //Last column
+                    if (x == xSize - 1)
+                        room.Walls[wallPositions.EastWall] = Wall.Vertical;
+                    //Last row
+                    if (y == ySize - 1)
+                        room.Walls[wallPositions.SouthWall] = Wall.Vertical;
+
+                }
+            }
+            return room
+        }
+    }
+}
